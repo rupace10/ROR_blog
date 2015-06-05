25d13261ac2dcfa612275265bf7bb211faee9fa6
Rupesh Maharjan <mhr.rupesh10@gmail.com>
2015-06-05 19:49:28 +0545

    removed moderator role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.role? :administrator
         can :manage, :all
-    elsif user.role? :moderator
-        can :manage, Comment
-        can :read, :all
     else
-        can :manage, Article
         can :read, :all
     end
   end
+
 end
